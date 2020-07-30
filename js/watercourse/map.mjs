e5d2acc7db2346ed51d6921b847de4cdbf739b41
Attila Oláh <atl@google.com>
2020-07-30 19:43:15 +0200

    Add fit bounds button.

--- js/watercourse/map.mjs
+++ js/watercourse/map.mjs
@@ -6,6 +6,15 @@ RS.centre = [
   (RS.bounds[0][1] + RS.bounds[1][1]) / 2,
 ];
 
+let map = null;
+let bounds = RS.bounds;
+
+function fitBounds() {
+  map.fitBounds(bounds, {
+    padding: 24,
+  });
+}
+
 export function CreateMap() {
   mapboxgl.accessToken = "pk.eyJ1IjoiYXR0aWxhb2xhaCIsImEiOiJVUXVXOXBBIn0.3kVsQJB-q0rnLfbmxvM-zg";
 
@@ -27,25 +36,54 @@ export function CreateMap() {
     }
   }
 
-  const map = new mapboxgl.Map({
+  class FitBoundsControl {
+    onAdd(map) {
+      this._map = map;
+
+      this._container = document.createElement("div");
+      this._container.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+
+      const btn = document.createElement("button");
+      btn.addEventListener("click", fitBounds);
+      this._container.appendChild(btn);
+
+      const i = document.createElement("i");
+      i.className = "material-icons fit";
+      i.innerText = "zoom_out_map";
+      btn.appendChild(i);
+
+      return this._container;
+    }
+
+    onRemove() {
+      this._map = undefined;
+      this._container.parentNode.removeChild(this._container);
+    }
+
+    getDefaultPosition() {
+      return "bottom-right";
+    }
+  }
+
+  map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/outdoors-v11",
     center: RS.centre,
     zoom: 5,
   });
-  map.on("load", () => {
-    map.fitBounds(RS.bounds);
-  });
   map.addControl(new FullscreenControl());
+  map.addControl(new FitBoundsControl());
+  map.on("load", fitBounds);
+
+  window.MAP = map;
 
   return map;
 }
 
 export function InitMap(map, data) {
   const init = () => {
-    map.fitBounds(turf.bbox(data), {
-      padding: 24,
-    });
+    bounds = turf.bbox(data);
+    fitBounds();
     map.addSource("river", {
       type: "geojson",
       data: data,
